9caa08f65906b8dc63c7f55558f500cebd335bae
loustrong <loustrong@163.com>
2019-10-27 23:20:13 +0800

    2nd chapter

--- 01-HelloWorld.js
+++ 01-HelloWorld.js
@@ -29,3 +29,33 @@ Book.prototype.printTitle = function(){
 
 book.printTitle();
 book.printIsbn();
+
+// let circleAreaES5 = function circleArea(r){
+//     const PI = 3.14;
+//     const area = PI * r * r;
+//     return area;
+// }
+// console.log(circleAreaES5(2));
+
+const circleArea = r =>{
+    const PI = 3.14;
+    const area = PI * r * r;
+    return area;
+}
+console.log(circleArea(2));
+
+class NewBook {
+    constructor(title, pages, isbn){
+        this.title = title;
+        this.pages = pages;
+        this.isbn = isbn;
+    }
+    printIsbn() {
+        console.log(this.isbn);
+    }
+}
+
+let newBook = new NewBook('title','pag','isbn');
+console.log(newBook.title);
+book.title = 'new title';
+console.log(book.title);
---
+++ 17-ES2015-ES6-Module.js
@@ -0,0 +1,4 @@
+import { circleArea as circle, squareArea as square } from   './17-CalcArea.js';
+
+console.log(circle(2));
+console.log(square(2));
